e6972d7499b15964adab37d9fe43b70e4d5a7ecf
Morgan Drake <12264057+modrake@users.noreply.github.com>
2022-07-26 10:27:09 -0700

    improvements to artifact name parsing

--- action/metadata-support.go
+++ action/metadata-support.go
@@ -2,7 +2,9 @@ package main
 
 import (
 	"context"
+	"fmt"
 	"os"
+	"regexp"
 	"strings"
 
 	"github.com/google/go-github/v45/github"
@@ -14,7 +16,7 @@ import (
 // artifact names paired with their variant type.
 // org, repo, and workflowrunID are the github vars passed in to the API for the
 // workflow run query, see: https://docs.github.com/en/rest/actions/artifacts#list-workflow-run-artifacts
-func getArtifacts(org string, repo string, workflowRunID int64) [][]string {
+func getArtifacts(org string, repo string, workflowRunID int64) map[string][]string {
 
 	// Auth to github API.
 	token := os.Getenv("CRT_GITHUB_TOKEN")
@@ -32,7 +34,7 @@ func getArtifacts(org string, repo string, workflowRunID int64) [][]string {
 	// Query API for list of artifacts associated with specified build workflow of product.
 	artifacts, _, err := ghClient.Actions.ListWorkflowRunArtifacts(ctx, org, repo, workflowRunID, &github.ListOptions{})
 	if err != nil {
-		githubactions.Errorf(err)
+		fmt.Println(err)
 	}
 
 	// Extract a list of artifact names from the artifact data structure returned from the
@@ -45,46 +47,124 @@ func getArtifacts(org string, repo string, workflowRunID int64) [][]string {
 	// Parse raw list of artifact names into struct of Artifacts and variant names.
 	// For Vault products, associate variant types with their artifacts. All other products
 	// do not require variant grouping and thus are associated with variant type "all".
-	var processedArtifacts [][]string
-	if repo == "vault-enterprise" {
-		for i := range rawArtifacts {
-			switch {
-			case strings.Contains(rawArtifacts[i], "ent.hsm.fips"):
-				processedArtifacts = append(processedArtifacts, []string{"ent.hsm.fips", rawArtifacts[i]})
-			case strings.Contains(rawArtifacts[i], "ent.fips"):
-				processedArtifacts = append(processedArtifacts, []string{"ent.fips", rawArtifacts[i]})
-			case strings.Contains(rawArtifacts[i], "ent.hsm"):
-				processedArtifacts = append(processedArtifacts, []string{"ent.hsm", rawArtifacts[i]})
-			case strings.Contains(rawArtifacts[i], "ent"):
-				processedArtifacts = append(processedArtifacts, []string{"ent", rawArtifacts[i]})
-			default:
-				githubactions.Infof("File %v does not match any expected pattern.", rawArtifacts[i])
-			}
-		}
-	} else if repo == "consul-k8s" {
-		for i := range rawArtifacts {
-			switch {
-			case strings.Contains(rawArtifacts[i], "consul-k8s-control-plane"):
-				processedArtifacts = append(processedArtifacts, []string{"consul-k8s-control-plane", rawArtifacts[i]})
-			case strings.Contains(rawArtifacts[i], "consul-k8s"):
-				processedArtifacts = append(processedArtifacts, []string{"consul-k8s", rawArtifacts[i]})
-			default:
-				githubactions.Infof("File %v does not match any expected pattern.", rawArtifacts[i])
-			}
-		}
-	} else {
-		// for all other products, we don't need to sort variants, but some builds include
-		// metadata files in their artifacts - this removes those from our final list.
-		for i := range rawArtifacts {
-			switch {
-			case strings.Contains(rawArtifacts[i], ".json") || strings.Contains(rawArtifacts[i], ".sig") ||
-				strings.Contains(rawArtifacts[i], "_SHA256SUMS"):
-				githubactions.Infof("File %v does not match any expected pattern.", rawArtifacts[i])
-			default:
-				processedArtifacts = append(processedArtifacts, []string{"all", rawArtifacts[i]})
-			}
+	processedArtifacts := make(map[string][]string)
+
+	// for all other products, we don't need to sort variants, but some builds include
+	// metadata files in their artifacts - this removes those from our final list.
+	for i := range rawArtifacts {
+		switch {
+		case strings.Contains(rawArtifacts[i], ".json") || strings.Contains(rawArtifacts[i], ".sig") ||
+			strings.Contains(rawArtifacts[i], "_SHA256SUMS"):
+			githubactions.Infof("File %v does not match any expected pattern.", rawArtifacts[i])
+		default:
+			productName := extractProductName(rawArtifacts[i])
+			processedArtifacts[productName] = append(processedArtifacts[productName], rawArtifacts[i])
 		}
 	}
 
 	return processedArtifacts
+}
+
+// extractProductName accepts an artifact name, ie. "product_1.2.3-ent_amd64.deb" and
+// returns a string containing the name/version of the product, ie. "product_1.2.3-ent"
+func extractProductName(rawArtifactName string) string {
+	// Extract the product name and relevant extentions with a big ugly regex. Basically,
+	// these regexes look for the semver in their input string, then search for a delimater
+	// between the variant strings that follow semver (-dev, +ent, +ent.hsm, etc.) and the
+	// architecture/file exention. The specifics of this search vary a little for a few special
+	// cases. Finally, we return the match minus the arch/file extenion.
+	productName := ""
+	// RPMs use a . instead of a _ to delinate their extentions.
+	if strings.Contains(rawArtifactName, "rpm") {
+		re := regexp.MustCompile(`^.*(0|[1-9]\d*)\.(0|[1-9]\d*)\.(0|[1-9]\d*).*?[^\.]*`)
+		productName = re.FindString(rawArtifactName)
+		productName = correctProductNameRPM(productName)
+	} else if strings.Contains(rawArtifactName, "docker") {
+		re := regexp.MustCompile(`^.*(0|[1-9]\d*)\.(0|[1-9]\d*)\.(0|[1-9]\d*).*?[^_]*`)
+		productName = re.FindString(rawArtifactName)
+		productName = correctProductNameDocker(productName)
+	} else {
+		re := regexp.MustCompile(`^.*(0|[1-9]\d*)\.(0|[1-9]\d*)\.(0|[1-9]\d*).*?[^_]*`)
+		productName = re.FindString(rawArtifactName)
+	}
+
+	// We need to do some post-processing to make the product name we use in our data more
+	// consistent than the product names we have in our artifacts:
+	// some files use ~ instead of - in their product text, we replace here as we want product
+	// names to stick to the - format.
+	productName = strings.ReplaceAll(productName, "~", "-")
+
+	// Some artifact names have a -1 after the semver, we want to remove that for our product
+	// name.
+	if len(productName) > 1 {
+		if productName[len(productName)-2:] == "-1" {
+			productName = productName[:len(productName)-2]
+		}
+	}
+
+	return productName
+}
+
+// Docker filenames look something like "product_default_amd_linux_386_1.2.3-dev".
+// correctProductNameDocker parses these names and returns a product name which fits
+// the norms for our other artifacts, such as "product_1.2.3-dev" for the example.
+func correctProductNameDocker(productName string) string {
+	// List of strings indicitive of docker cruft - add new strings here if a team
+	// changes their docker naming and their artifacts aren't getting caught.
+	// Map of structs is a little odd looking, but allows easy lookups as we loop
+	// through the terms in productName, preventing an On^2 loop.
+	var dockerExceptions = map[string]struct{}{
+		"default": {}, "release": {}, "release-default": {}, "ubi": {},
+	}
+	// Split the original string along the delimeter "_", then loop through 1 word at a time.
+	substrings := strings.Split(productName, "_")
+	var newProductName string
+	for i, word := range substrings {
+		// When we find a term that signifies docker filename cruft, loop through the
+		// remainder of the list looking for the semver and setting anything else to an
+		// empty string. When we find the semver, break out of this loop.
+		if _, ok := dockerExceptions[word]; ok {
+			for j := i; j < len(substrings); j++ {
+				re := regexp.MustCompile(`(0|[1-9]\d*)\.(0|[1-9]\d*)\.(0|[1-9]\d*).*?[^_]*`)
+				found := re.MatchString(substrings[j])
+				if found {
+					break
+				} else {
+					substrings[j] = ""
+				}
+			}
+		}
+		// build a new string from the current substring index, ignoring empty strings and
+		// re-inserting the _ which we removed as a delimeter.
+		if i == 0 {
+			newProductName += substrings[i]
+		} else if substrings[i] != "" {
+			newProductName = newProductName + "_" + substrings[i]
+		}
+	}
+
+	return newProductName
+}
+
+// rpms use a - instead of an _ as the delimeter before the semver. This changes it to
+// "_" for consistency.
+func correctProductNameRPM(productName string) string {
+	// Solving this required some hard to read logic, documenting heavily for readability:
+	// Create a regex that finds a substring from start of string through semver.
+	re := regexp.MustCompile(`^.*-(0|[1-9]\d*)\.(0|[1-9]\d*)\.(0|[1-9]\d*)`)
+	// Break the product name into 2 halves with semver as a fulcrum. This produces 2
+	// halves that look something like: "product-1.2.3" and "-dev-1" or
+	// "product-1.2.3" and "".
+	prefixString := strings.Join(re.FindAllString(productName, -1), "")
+	postfixString := strings.Join(re.Split(productName, -1), "")
+	// Find the last "-" in the prefix substring, verify that it exists, and rebuild the
+	// prefix string around that index replacing "-" with "_"
+	charIndex := strings.LastIndex(prefixString, "-")
+	if charIndex > 0 {
+		prefixString = prefixString[:charIndex] + "_" + prefixString[charIndex+1:]
+	} else {
+		githubactions.Warningf("Attempt to correct RPM product name for consistency failed.")
+	}
+	// rebuild the original product name using the modified prefix.
+	return prefixString + postfixString
 }
